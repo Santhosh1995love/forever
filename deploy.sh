794524347c4f118a1125caa3d7ec9eb00df77bce
Santhosh1995love <157947117+Santhosh1995love@users.noreply.github.com>
2024-06-28 23:14:10 +0530

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ $GIT_BRANCH == "origin/Dev" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u Santhosh1995love -p dckr_pat_HFQjmql_Mgbprub42SMVKz2573Q
+    docker tag devops-build Santhosh1995love/dev
+    docker push Santhosh1995love/dev
+
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u Santhosh1995love -p dckr_pat_HFQjmql_Mgbprub42SMVKz2573Q
+    docker tag devops-build Santhosh1995love/prod 
+    docker push Santhosh1995love/prod
+else
+    echo "Failed"
+fi
