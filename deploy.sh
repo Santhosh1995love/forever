f999ff0479cf07d8ddacd04e63075883522000f0
Santhosh1995love <157947117+Santhosh1995love@users.noreply.github.com>
2024-06-29 00:04:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u Santhosh1995love -p dckr_pat_HFQjmql_Mgbprub42SMVKz2573Q
-    docker tag devops-build Santhosh1995love/dev
-    docker push Santhosh1995love/dev
+    docker login -u santysanthosh1995 -p dckr_pat_HFQjmql_Mgbprub42SMVKz2573Q
+    docker tag devops-build santysanthosh1995/dev
+    docker push santysanthosh1995/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u Santhosh1995love -p dckr_pat_HFQjmql_Mgbprub42SMVKz2573Q
-    docker tag devops-build Santhosh1995love/prod 
-    docker push Santhosh1995love/prod
+    docker login -u santysanthosh1995 -p dckr_pat_HFQjmql_Mgbprub42SMVKz2573Q
+    docker tag devops-build santysanthosh1995/prod 
+    docker push santysanthosh1995/prod
 else
     echo "Failed"
 fi
